19d58f16ab259f4bfe2bf789e60e398978d3f74b
jcayetano <jcayetano@apap.com.do>
2021-07-02 22:14:45 -0400

    scanning qr

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  result: string;
+
+  swiperOpts = {
+    allowSlidePrev: false,
+    allowSlideNext: false,
+  }
+
+  constructor(private scanner: BarcodeScanner) { }
+  ngOnInit(): void {
+  }
+
+  ionViewWillEnter() {
+    console.log('Va a entrar');
+    this.startScan();
+  }
+
+  startScan() {
+    this.scanner.scan().then(data => {
+      console.log('Data', data);
+      this.result = data.text;
+    }).catch(err => {
+      console.log('Error');
+    });
+  }
 
 }
